da6ceda4cef332edc97549c1d8f0ae3b0a299bb4
Isaac Robert <isaacrobertoluwaseun@gmail.com>
2023-03-13 13:02:02 +0100

    Password conn test

---
+++ tests/test_password_conn.py
@@ -0,0 +1,20 @@
+from channels_redis_contrib.core import RedisChannelLayer
+from dotenv import load_dotenv
+import pytest, os
+
+load_dotenv()
+
+TEST_HOSTS = ["redis://{os.getenv('REDIS_HOST)}:6379"]
+PASSWORD = os.getenv("REDIS_PASSWORD")
+
+
+@pytest.fixture()
+async def channel_layer():
+    """
+    Channel layer fixture that flushes automatically.
+    """
+    channel_layer = RedisChannelLayer(
+        hosts=TEST_HOSTS, capacity=3, channel_capacity={"tiny": 1}, password=PASSWORD
+    )
+    yield channel_layer
+    await channel_layer.flush()
